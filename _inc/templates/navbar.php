00677fd9029f2c3bafdd31f3a9236e6286bbc09b
MaheKarim <mahekarim@gmail.com>
2019-10-04 22:01:11 +0600

    Index File

---
+++ _inc/templates/navbar.php
@@ -0,0 +1,5 @@
+<p>
+     <a href="crud-php/index.php/task=report"> All Students </a>
+     <a href="crud-php/index.php/task=add"> Add New Student </a>
+     <a href="crud-php/index.php/task=seed"> Seed </a>
+</p>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Miligram CSS -->
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.css">
+    <link rel="stylesheet" href="_inc/style.css">
+    <title>PHP CRUD</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="column column-60 column-offset-20">
+                <h2>Project CRUD</h2>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti molestias repellendus 
+                    minus. Nulla, tenetur quisquam commodi impedit inventore harum ad, soluta adipisci iure 
+                    facilis, culpa non distinctio beatae consequuntur assumenda. </p>
+                    <?php include_once('_inc/templates/navbar.php'); ?>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
